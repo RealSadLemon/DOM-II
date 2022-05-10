ee2a3907f6f193969bc7c16e13c015ae2da721e3
RealSadLemon <esmodearburk@gmail.com>
2022-05-10 15:07:13 -0500

    Added event listeners for the following: all images lead to Rick Astley's Never gonna give you up, html changes color on keypress of w or r, and footer changes background color on mouseenter and mouseleave

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './less/index.less'
-
+(()=>{
+    const html = document.querySelector('html');
+    const footer = document.querySelector('.footer');
+    const imagesArr = document.querySelectorAll('img');
+    html.addEventListener('keydown', e =>{
+        if(e.key === 'r'){
+            html.style.backgroundColor = 'red';
+        }else if(e.key === 'w'){
+            html.style.backgroundColor = 'white';
+        }
+    });
+    footer.addEventListener('mouseenter', e =>{
+        footer.style.backgroundColor = '#000000';
+    });
+    footer.addEventListener('mouseleave', e =>{
+        footer.style.backgroundColor = '#FFEBCD';
+    });
+    imagesArr.forEach(image => {
+        image.addEventListener('click', e =>{
+            window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
+        });
+    });
+    
+})()
 // Your code goes here!
